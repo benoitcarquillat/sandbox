56d328523b2e7472491627a1956ce482e5c52135
Carquillat <benoit.carquillat@gmail.com>
2022-03-22 15:50:02 +0100

    feat: add texture img

--- src/App.js
+++ src/App.js
@@ -1,32 +1,76 @@
-import { Canvas, extend } from '@react-three/fiber';
+import * as THREE from 'three';
+import { useRef, Suspense } from 'react';
+import { Canvas, extend, useFrame, useLoader } from '@react-three/fiber';
 import { shaderMaterial } from '@react-three/drei';
 import glsl from 'babel-plugin-glsl/macro';
 
 const WaveShaderMaterial = shaderMaterial(
-  {},
+  {
+    uTime: 0,
+    uColor: new THREE.Color(0.0, 0.0, 0.0),
+    uTexture: new THREE.Texture(),
+  },
   glsl`
+    precision mediump float;
+    uniform float uTime;
+
+    varying vec2 vUv;
+
+    #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
+
     void main(){
+      vUv = uv;
+
+      vec3 pos = position;
+      float noiseFreq = 1.5;
+      float noiseAmp = 0.25;
+      vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
+      pos.z += snoise3(noisePos) * noiseAmp;
+
       gl_Position = projectionMatrix * modelViewMatrix * vec4
-      (position, 1.0);
+      (pos, 1.0);
     }
 `,
+  // Fragment shader
   glsl`
+    precision mediump float;
+
+    uniform vec3 uColor;
+    uniform float uTime;
+    uniform sampler2D uTexture;
+
+    varying vec2 vUv;
+
     void main() {
-      gl_FragColor = vec4(0.0, 0.4, 1.0, 1.0);
+      vec3 texture = texture2D(uTexture, vUv).rgb;
+      gl_FragColor = vec4(texture, 1.0);
     }
   `,
 );
 
 extend({ WaveShaderMaterial });
 
+const Wave = () => {
+  const ref = useRef(null);
+  useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
+
+  const [image] = useLoader(THREE.TextureLoader, [
+    'https://picsum.photos/500/700',
+  ]);
+  return (
+    <mesh>
+      <planeBufferGeometry args={[0.4, 0.6, 16, 16]} />
+      <waveShaderMaterial ref={ref} uColor={'tomato'} uTexture={image} />
+    </mesh>
+  );
+};
+
 const Scene = () => {
   return (
-    <Canvas>
-      <mesh>
-        <pointLight position={[10, 10, 10]} />
-        <planeBufferGeometry args={[3, 5]} />
-        <waveShaderMaterial />
-      </mesh>
+    <Canvas camera={{ fov: 12, position: [0, 0, 5] }}>
+      <Suspense fallback={null}>
+        <Wave />
+      </Suspense>
     </Canvas>
   );
 };
